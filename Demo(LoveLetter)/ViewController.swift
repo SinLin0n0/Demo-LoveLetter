d4faf863065fa536372fbc4284520964c00ed46c
Sin Lin <sinlin0n0@gmail.com>
2023-01-06 13:32:15 +0800

    Initial Commit

---
+++ Demo(LoveLetter)/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Demo(LoveLetter)
+//
+//  Created by Sin on 2023/1/6.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
